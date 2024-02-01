24f022eb299e1b83eefb3c96e198d1cf717981bb
William Tucker <william.tucker29@gmail.com>
2024-01-31 20:31:35 -0600

    Working on Rooks code

--- src/solvers.js
+++ src/solvers.js
@@ -20,6 +20,30 @@ window.findNRooksSolution = function(n) {
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
+
+  var board = new Board({n: n});
+  var inner = function(n, startingRow, startingCol) {
+    var startingRow = startingRow || 0;
+    var startingCol = startingCol || 0;
+
+    var rooksLeft = rooksLeft || n;
+    var currentRow = currentRow || 0;
+    var currentCol = currentCol || 0;
+
+    if (rooksLeft === 0) {
+      return;
+    }
+    if (currentRow === n) {
+      return;
+    }
+    if (!this.hasColConflictAt(currentCol) && !this.hasRowConflictAt(currentRow)) {
+      board.togglePiece(currentRow, currentCol);
+      rooksLeft--;
+    }
+    if (currentCol === n - 1) {
+
+    }
+  };
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
